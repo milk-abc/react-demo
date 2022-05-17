326bb0cd49fa86ac40c4a2757db8c9a742e1aaba
milk-abc <3292944779@qq.com>
2022-05-17 15:18:27 +0800

    feat: 轮流落子

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -15,15 +15,13 @@ import "./index.css";
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-class Square extends React.Component {
+function Square(props) {
   //单元格
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -32,12 +30,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
   renderSquare(i) {
     return (
@@ -48,7 +47,7 @@ class Board extends React.Component {
     );
   }
   render() {
-    const status = "Next player:X";
+    const status = `Next player:${this.state.xIsNext ? "X" : "O"}`;
     return (
       <div>
         <div className="status">{status}</div>
